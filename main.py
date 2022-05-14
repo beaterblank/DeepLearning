b14357805236c85cada5a64bb2e2a6c541b1a85e
G.Mohan Teja <mohanteja.g2019@vitstudent.ac.in>
2022-05-14 18:24:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import numpy as np
+#sigmod function
+def sigmod(z):
+    return 1.0/(1.0+np.exp(-z))
+#neural network object
+class Network(object):
+    #list sizes contains the number of neurons in the respective layers
+    def __init__(self,sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        #np.random.randn(d0,d1,....,dn) gives a random guassian
+        #distributed (mean:0,sd:1) array of dimensions  d0,d1,....,dn
+        #initiate weights and biases using this
+        self.biases  = [np.random.randn(y,1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y,x) for x,y in zip(sizes[:-1],sizes[1:])]
+
+    def feedforward(self, a):
+        """Return the output of the network if "a" is input."""
+        for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+        return a
+
+    def SGD(self, training_data, epochs, mini_batch_size, eta,
+        test_data=None):
+        """Train the neural network using mini-batch stochastic
+        gradient descent.  The "training_data" is a list of tuples
+        "(x, y)" representing the training inputs and the desired
+        outputs.  The other non-optional parameters are
+        self-explanatory.  If "test_data" is provided then the
+        network will be evaluated against the test data after each
+        epoch, and partial progress printed out.  This is useful for
+        tracking progress, but slows things down substantially."""
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in xrange(epochs):
+            random.shuffle(training_data)
+            mini_batches = [training_data[k:k+mini_batch_size] for k in xrange(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                self.update_mini_batch(mini_batch, eta)
+            if test_data:
+                print "Epoch {0}: {1} / {2}".format(
+                    j, self.evaluate(test_data), n_test)
+            else:
+                print "Epoch {0} complete".format(j)
